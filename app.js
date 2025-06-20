55848b1934bff96195172babd84693eca43908b5
M19Y <muhamadarsilalhabsy@gmail.com>
2025-06-20 17:14:17 +0800

    change app.js, update db and update security
    update db on app.js
    
    update security on app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("App running on ticket-22");
 console.log("Server running on port 22")
-console.log("App running on ticket-11");
+console.log("DB using MySQL")
+console.log("Security Using JWT")
